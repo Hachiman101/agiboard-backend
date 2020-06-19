96eae4a1451dad6639d6744a3c8a45e0a9fac6bc
Hachiman101 <12989770+Hachiman101@users.noreply.github.com>
2020-06-19 19:37:22 +0200

    Add TestController

--- src/main/java/com/kanboard/TestController.java
+++ src/main/java/com/kanboard/TestController.java
@@ -1,19 +1,43 @@
 package com.kanboard;
 
+import com.kanboard.entity.User;
+import com.kanboard.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/test")
 public class TestController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping
     public ResponseEntity<String> Test() {
         String testing = "Testing API";
         return ResponseEntity.status(200).body(testing);
+    }
+
+    @GetMapping("/create")
+    public ResponseEntity<User> create() {
+        User user = new User();
+        user.setName("name");
+        user.setUsername("username");
+        user.setPassword("password");
+        user.setBoards(new ArrayList<>());
+
+        userRepository.save(user);
+        User userReturn = userRepository.findByUsername(user.getUsername());
+        return ResponseEntity.status(200).body(userReturn);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<User> get() {
+        User userReturn = userRepository.findByUsername("username");
+        return ResponseEntity.status(200).body(userReturn);
     }
 }
