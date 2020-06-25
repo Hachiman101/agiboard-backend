631d13f4ae18521d500da18ef401116c71178e0d
Hachiman101 <12989770+Hachiman101@users.noreply.github.com>
2020-06-25 16:19:06 +0200

    Remove signing key

--- src/main/java/com/kanboard/util/JwtTokenUtil.java
+++ src/main/java/com/kanboard/util/JwtTokenUtil.java
@@ -18,7 +18,7 @@ import java.util.function.Function;
 @Component
 public class JwtTokenUtil implements Serializable {
     public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
-    public static final String SIGNING_KEY = "%NdXNoB%z^p^2^!x$kaUPjTu#AgAsPRpUrc";
+    public static final String SIGNING_KEY = System.getenv("SIGNING_KEY");
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 
